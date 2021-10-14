191113af9d7ed971bc95e91b0d694d59e5a1753a
unknown <zackniyokwizera>
2021-10-14 17:26:30 +0200

    added DOMcontentLoaded method

--- app.js
+++ app.js
@@ -1,118 +1,134 @@
-//Book class: represents a book
+// Book Class: Represents a Book
 class Book {
-    constructor(title, author, isbn) {
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-    }
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
 }
 
-
-// UI Class: Handle UI tasks.
+// UI Class: Handle UI Tasks
 class UI {
+  static displayBooks() {
+    const books = Store.getBooks();
 
-    // Add a book
-    static addBookToList(book) {
+    books.forEach((book) => UI.addBookToList(book));
+  }
 
-        const list = document.querySelector('#book-list');
+  static addBookToList(book) {
+    const list = document.querySelector("#book-list");
 
-        const row = document.createElement('tr');
+    const row = document.createElement("tr");
 
-        row.innerHTML = `<td>${book.title}</td>
-                <td>${book.author}</td>
-                <td>${book.isbn}</td>
-                <td>
-                <a href="#" class="btn-danger btn-sm delete">X</a>
-                </td>`;
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+    `;
 
-        list.appendChild(row);
+    list.appendChild(row);
+  }
+
+  static deleteBook(el) {
+    if (el.classList.contains("delete")) {
+      el.parentElement.parentElement.remove();
     }
+  }
 
-    //Deleting a book
+  static showAlert(message, className) {
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const form = document.querySelector("#book-form");
+    container.insertBefore(div, form);
 
-    static deleteBook(el) {
+    // Vanish in 3 seconds
+    setTimeout(() => document.querySelector(".alert").remove(), 3000);
+  }
 
-        if (el.classList.contains('delete')) {
-
-            el.parentElement.parentElement.remove();
-
-        }
-
-    }
-
-    //clear  form Method 
-
-    static clearFormFields() {
-        const title = document.getElementById('title').value = ' ';
-        const author = document.getElementById('author').value = ' ';
-        const isbn = document.getElementById('isbn').value = ' ';
-    }
-
-
-    // showing the alert 
-
-    static showAlertMessage(message, className) {
-
-        const div = document.createElement('div');
-        div.className = `alert alert-${className}`;
-        div.appendChild(document.createTextNode(message));
-
-        const container = document.querySelector('.container');
-        const form = document.querySelector('#book-form');
-        container.insertBefore(div, form);
-
-
-
-
-        function myFunction() {
-            setTimeout(
-
-                function() {
-
-                    let removeMessage = document.querySelector('.alert');
-                    removeMessage.remove();
-
-                },
-                2000);
-        }
-
-        myFunction();
-
-    }
+  static clearFields() {
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#isbn").value = "";
+  }
 }
 
-document.querySelector('#book-form').addEventListener('submit', (e) => {
+// Store Class: Handles Storage
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
 
-    //prevent default
-    e.preventDefault();
+    return books;
+  }
 
-    //A. access our book 
-    //A1. Get form values
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    const isbn = document.getElementById('isbn').value;
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
 
-    // console.log(title, author, isbn);
+  static removeBook(isbn) {
+    const books = Store.getBooks();
 
-    //A2. creating a new book Object 
+    books.forEach((book, index) => {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
 
-    book = new Book(title, author, isbn);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+}
 
+// Event: Display Books
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
+
+// Event: Add a Book
+document.querySelector("#book-form").addEventListener("submit", (e) => {
+  // Prevent actual submit
+  e.preventDefault();
+
+  // Get form values
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
+
+  // Validate
+  if (title === "" || author === "" || isbn === "") {
+    UI.showAlert("Please fill in all fields", "danger");
+  } else {
+    // Instatiate book
+    const book = new Book(title, author, isbn);
+
+    // Add Book to UI
     UI.addBookToList(book);
 
-    UI.clearFormFields();
+    // Add book to store
+    Store.addBook(book);
 
-    UI.showAlertMessage('Book Added', 'info');
+    // Show success message
+    UI.showAlert("Book Added", "success");
 
-})
+    // Clear fields
+    UI.clearFields();
+  }
+});
 
-document.querySelector('#book-list').addEventListener('click', (e) => {
+// Event: Remove a Book
+document.querySelector("#book-list").addEventListener("click", (e) => {
+  // Remove book from UI
+  UI.deleteBook(e.target);
 
-    //deleting a book from UI
+  // Remove book from store
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
-    UI.deleteBook(e.target);
-    console.log(e.target);
-
-
-    UI.showAlertMessage('Book Deleted', 'danger');
-})
+  // Show success message
+  UI.showAlert("Book Removed", "success");
+});
